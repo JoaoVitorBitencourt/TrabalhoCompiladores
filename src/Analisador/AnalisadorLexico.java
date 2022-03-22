5da3f821d4dbf18a7e6648782436d768b67e56b0
João <joaos_vb@hotmail.com>
2022-03-21 22:06:13 -0300

    Corrigido alguns erros de leitura

--- src/Analisador/AnalisadorLexico.java
+++ src/Analisador/AnalisadorLexico.java
@@ -8,22 +8,14 @@ import Gramatica.Token;
 public class AnalisadorLexico {
     
     public Stack<Token> gerarTokens(String programa){
-        String[] tokens = programa.split(" ");
-        System.out.println(tokens);
-        Stack<Token> pilhaTokens = new Stack<>();
-        Stack<Token> teste = getListaTokens(programa);
-        
-        for(int i = tokens.length -1; i >= 0; i--) {
-            String token = tokens[i];
-            if(token.trim().length() == 0) {
-                continue;
-            }
-            Integer codigo = getCodigoToken(token);
-            pilhaTokens.push(new Token(token, codigo));
+        Stack<Token> pilhaInversa = new Stack<>();
+        Stack<Token> pilhaTokens = getListaTokens(programa);
+
+        while(!pilhaTokens.empty()){
+            pilhaInversa.push(pilhaTokens.pop());
         }
         
-        
-        return pilhaTokens;
+        return pilhaInversa;
     }
 
     private Stack<Token> getListaTokens(String programa) {
@@ -34,7 +26,10 @@ public class AnalisadorLexico {
             String charAtual = Character.toString(programa.charAt(i));
 
             if(i+1 >= programa.length()) { // trata fim do arquivo
-                if(!charAtual.trim().equals("")){
+                if(!token.trim().equals("")) { // verifica se há algum token para armazenar antes de finalizar
+                    pilhaTokens.push(new Token(token, getCodigoToken(token)));
+                }
+                if(!charAtual.trim().equals("")){//grava o caracter final do programa se não for nullo ou espaço
                     pilhaTokens.push(new Token(charAtual, getCodigoToken(charAtual)));
                 }
                 break;
@@ -42,7 +37,7 @@ public class AnalisadorLexico {
 
             String validador = charAtual + Character.toString(programa.charAt(i+1));
 
-            switch(validador){
+            switch(validador.toUpperCase()){
                 case "(*": 
                     for(int j = i; j <= programa.length() -1 ; j++) {
                         String charComentarioAnt = Character.toString(programa.charAt(j)) + programa.charAt(j+1);
@@ -56,14 +51,51 @@ public class AnalisadorLexico {
                 case "<=":
                 case ">=":
                 case "<>":
+                case "..":
+                    if(!token.trim().equals("")) {
+                        pilhaTokens.push(new Token(token, getCodigoToken(token)));
+                        token = "";
+                    }
                     pilhaTokens.push(new Token(validador, getCodigoToken(validador)));
+                    i++;
                 break;
                 default:
                     switch(charAtual) {
+                        case "'": 
+                        case "`":
+                        case "´":
+                        case "‘":
+                            String stringCod = new String();
+                            stringCod = charAtual; // grava char atual e começa o for a partir do proximo
+
+                            for(int j = i + 1; j <= programa.length() -1 ; j++) {
+                                String charStringAtual = Character.toString(programa.charAt(j));
+
+                                if(!(charStringAtual.equals("'") || charStringAtual.equals("`") || charStringAtual.equals("´") || charStringAtual.equals("‘"))) {
+                                    stringCod += charStringAtual;
+                                } else {
+                                    stringCod += charStringAtual;
+                                    pilhaTokens.push(new Token(stringCod, getCodigoToken(stringCod)));
+                                    stringCod = "";
+                                    i = j;
+                                    break;
+                                }
+                            }
+                        break;
                         case ";": 
                         case " ":
                         case ",":
                         case ".":
+                        case ":":
+                        case "+":
+                        case "-":
+                        case "*":
+                        case "/":
+                        case "[":
+                        case "]":
+                        case "(":
+                        case ")":
+                        case "=":
                             if(!token.trim().equals("")) { 
                                 pilhaTokens.push(new Token(token, getCodigoToken(token))); // grava token
                                 token = "";
@@ -85,7 +117,7 @@ public class AnalisadorLexico {
     
     private Integer getCodigoToken(String token){
         
-        Integer codigoToken = Gramatica.DICIONARIO.get(token);
+        Integer codigoToken = Gramatica.DICIONARIO.get(token.toUpperCase());
         if(codigoToken == null) {
             //ou � um INTEGER ou pe um IDENTIFICADOR
             return getIdentificadorOuInteiro(token);
