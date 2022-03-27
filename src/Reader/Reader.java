c93603cf809f40b4d0367574b1ee4fc24dd9e357
Flavio-LP <flavio5070@outlook.com>
2022-03-27 16:08:05 -0300

    num alterei nada

--- src/Reader/Reader.java
+++ src/Reader/Reader.java
@@ -2,11 +2,11 @@ package Reader;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
+//import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+//import java.io.InputStream;
+//import java.io.InputStreamReader;
 
 public class Reader {
 	
